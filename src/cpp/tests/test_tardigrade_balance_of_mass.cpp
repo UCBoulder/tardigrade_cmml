845aa5161262da337d68ea2efd816233b36a694d
Nathan Miller <nami2227@colorado.edu>
2024-12-11 15:36:53 -0700

    WIP: Starting to add tests for the FEA form of the balance of mass

--- src/cpp/tests/test_tardigrade_balance_of_mass.cpp
+++ src/cpp/tests/test_tardigrade_balance_of_mass.cpp
@@ -205,6 +205,163 @@ BOOST_AUTO_TEST_CASE( test_computeBalanceOfMass, * boost::unit_test::tolerance( 
 
 }
 
+BOOST_AUTO_TEST_CASE( test_computeBalanceOfMass_fea, * boost::unit_test::tolerance( DEFAULT_TEST_TOLERANCE ) ){
+    /*!
+     * Test computing the balance of mass in a finite element context
+     */
+
+    floatType density = 1.4;
+
+    floatType density_dot = 2.8;
+
+    floatVector density_gradient = { 1., 2., 3. };
+
+    floatVector velocity = { 0.1, 0.2, 0.3 };
+
+    secondOrderTensor velocity_gradient = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+
+    floatType answer = 6.3;
+
+    floatType result;
+    
+    tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, velocity, velocity_gradient, result );
+
+    BOOST_TEST( answer == result );
+
+    floatType dCdRho, dCdRhoDot;
+
+    floatVector dCdGradRho, dCdV;
+
+    secondOrderTensor dCdGradV;
+
+    result = 0.;
+
+    tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, velocity, velocity_gradient, result,
+                                                                     dCdRho,  dCdRhoDot,   dCdGradRho      , dCdV,     dCdGradV );
+
+    BOOST_TEST( answer == result );
+
+    floatType eps = 1e-6;
+
+    // Derivative w.r.t. density
+    for ( unsigned int i = 0; i < 1; i++ ){
+
+        floatType delta = eps * std::fabs( density ) + eps;
+
+        floatType xp = density + delta;
+
+        floatType xm = density - delta;
+
+        floatType vp;
+
+        floatType vm;
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( xp, density_dot, density_gradient, velocity, velocity_gradient, vp );
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( xm, density_dot, density_gradient, velocity, velocity_gradient, vm );
+
+        BOOST_TEST( dCdRho == ( vp - vm ) / ( 2 * delta ) );
+
+    }
+
+    // Derivative w.r.t. density dot
+    for ( unsigned int i = 0; i < 1; i++ ){
+
+        floatType delta = eps * std::fabs( density_dot ) + eps;
+
+        floatType xp = density_dot + delta;
+
+        floatType xm = density_dot - delta;
+
+        floatType vp;
+
+        floatType vm;
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, xp, density_gradient, velocity, velocity_gradient, vp );
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, xm, density_gradient, velocity, velocity_gradient, vm );
+
+        BOOST_TEST( dCdRhoDot == ( vp - vm ) / ( 2 * delta ) );
+
+    }
+
+    // Derivative w.r.t. density gradient
+    for ( unsigned int i = 0; i < 3; i++ ){
+
+        floatType delta = eps * std::fabs( density_gradient[ i ] ) + eps;
+
+        floatVector xp = density_gradient;
+
+        floatVector xm = density_gradient;
+
+        xp[ i ] += delta;
+
+        xm[ i ] -= delta;
+
+        floatType vp;
+
+        floatType vm;
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, xp, velocity, velocity_gradient, vp );
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, xm, velocity, velocity_gradient, vm );
+
+        BOOST_TEST( dCdGradRho[ i ] == ( vp - vm ) / ( 2 * delta ) );
+
+    }
+
+    // Derivative w.r.t. velocity
+    for ( unsigned int i = 0; i < 3; i++ ){
+
+        floatType delta = eps * std::fabs( velocity[ i ] ) + eps;
+
+        floatVector xp = velocity;
+
+        floatVector xm = velocity;
+
+        xp[ i ] += delta;
+
+        xm[ i ] -= delta;
+
+        floatType vp;
+
+        floatType vm;
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, xp, velocity_gradient, vp );
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, xm, velocity_gradient, vm );
+
+        BOOST_TEST( dCdV[ i ] == ( vp - vm ) / ( 2 * delta ) );
+
+    }
+
+    // Derivative w.r.t. velocity gradient
+    for ( unsigned int i = 0; i < 9; i++ ){
+
+        floatType delta = eps * std::fabs( velocity_gradient[ i ] ) + eps;
+
+        secondOrderTensor xp = velocity_gradient;
+
+        secondOrderTensor xm = velocity_gradient;
+
+        xp[ i ] += delta;
+
+        xm[ i ] -= delta;
+
+        floatType vp;
+
+        floatType vm;
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, velocity, xp, vp );
+
+        tardigradeBalanceEquations::balanceOfMass::computeBalanceOfMass( density, density_dot, density_gradient, velocity, xm, vm );
+
+        BOOST_TEST( dCdGradV[ i ] == ( vp - vm ) / ( 2 * delta ) );
+
+    }
+
+}
+
 BOOST_AUTO_TEST_CASE( test_multiphase_computeBalanceOfMass, * boost::unit_test::tolerance( DEFAULT_TEST_TOLERANCE ) ){
     /*!
      * Test computing the balance of mass
